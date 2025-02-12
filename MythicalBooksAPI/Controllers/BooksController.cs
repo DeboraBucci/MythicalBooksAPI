2f4419c544a3d00ce7758f016cd03ffe691e51ea
Debora Bucci <dbucci@protonmail.com>
2025-02-12 17:30:34 -0300

    add controller for the books, with a request method for getting the books

---
+++ MythicalBooksAPI/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Data;
+using MythicalBooksAPI.Dtos;
+using MythicalBooksAPI.Mappers;
+
+namespace MythicalBooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = await _context
+                .Books
+                .Include(b => b.BookAuthors)
+                    .ThenInclude(ba => ba.Author)
+                .Include(b => b.BookCategories)
+                    .ThenInclude(bc => bc.Category)
+                .Include(b => b.BookPublishers)
+                    .ThenInclude(bp => bp.Publisher)
+                .Select(b => BookMapper.ToBookDto(b))
+                .ToListAsync();
+
+            return Ok(books);
+        }
+    }
+}
