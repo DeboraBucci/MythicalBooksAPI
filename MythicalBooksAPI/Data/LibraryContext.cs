85daf68061ba018e6e15de18e6b523ee5a02a6ef
Debora Bucci <dbucci@protonmail.com>
2025-02-12 14:27:49 -0300

    create a context and connect to a many-to-many relationship the entities

---
+++ MythicalBooksAPI/Data/LibraryContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Models.Entities;
+
+namespace MythicalBooksAPI.Data
+{
+    public class LibraryContext : DbContext
+    {
+
+        public LibraryContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<BookPublisher> BookPublishers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // Many-to-Many: Book - Author
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
+
+            // Many-to-Many: Book - Category
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Book)
+                .WithMany(b => b.BookCategories)
+                .HasForeignKey(bc => bc.BookId);
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Category)
+                .WithMany(c => c.BookCategories)
+                .HasForeignKey(bc => bc.CategoryId);
+
+            // Many-to-Many: Book - Publisher
+            modelBuilder.Entity<BookPublisher>()
+                .HasKey(bp => new { bp.BookId, bp.PublisherId });
+
+            modelBuilder.Entity<BookPublisher>()
+                .HasOne(bp => bp.Book)
+                .WithMany(b => b.BookPublishers)
+                .HasForeignKey(bp => bp.BookId);
+
+            modelBuilder.Entity<BookPublisher>()
+                .HasOne(bp => bp.Publisher)
+                .WithMany(p => p.BookPublishers)
+                .HasForeignKey(bp => bp.PublisherId);
+        }
+
+    }
+}
