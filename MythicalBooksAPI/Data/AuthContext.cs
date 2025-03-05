cdd4baee39767e7fc0d16a0b45065c1b022e81ef
Debora Bucci <dbucci@protonmail.com>
2025-03-05 19:12:51 -0300

    create context for the authentication

---
+++ MythicalBooksAPI/Data/AuthContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Models.Auth;
+using MythicalBooksAPI.Models.Books;
+
+namespace MythicalBooksAPI.Data
+{
+    public class AuthContext: DbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
+        public DbSet<UserSubscription> UserSubscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserSubscription>()
+                .HasOne(us => us.User)
+                .WithMany(us => us.UserSubscriptions)
+                .HasForeignKey(us => us.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<UserSubscription>()
+                .HasOne(us => us.SubscriptionPlan)
+                .WithMany(us => us.UserSubscriptions)
+                .HasForeignKey(us => us.SubscriptionPlanId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
