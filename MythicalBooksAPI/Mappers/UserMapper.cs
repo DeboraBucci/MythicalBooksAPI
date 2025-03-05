6388616f75ef433a2c8412e0aaf95ae2a83956fa
Debora Bucci <dbucci@protonmail.com>
2025-03-05 19:06:53 -0300

    create a mapper for turning a RegisterUserDto object into a User object

---
+++ MythicalBooksAPI/Mappers/UserMapper.cs
@@ -0,0 +1,25 @@
+﻿using MythicalBooksAPI.Dtos;
+using MythicalBooksAPI.Models.Auth;
+
+namespace MythicalBooksAPI.Mappers
+{
+    public static class UserMapper
+    {
+        public static User ToUser(this RegisterUserDto request, string hashedPassword)
+        {
+            return new User
+            {
+                Email = request.Email,
+                Password = hashedPassword,
+                Username = request.Username,
+                Name = request.Name,
+                Surname = request.Surname,
+                DateOfBirth = request.DateOfBirth,
+                Address = request.Address,
+                Country = request.Country,
+                City = request.City,
+                PostalCode = request.PostalCode
+            };
+        }
+    }
+}
