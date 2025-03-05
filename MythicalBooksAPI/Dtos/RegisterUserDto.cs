986e53a69dbb7a4428009d142c2869397a7b6aa9
Debora Bucci <dbucci@protonmail.com>
2025-03-05 19:06:19 -0300

    create dto for managing the data when resigtering a user

---
+++ MythicalBooksAPI/Dtos/RegisterUserDto.cs
@@ -0,0 +1,19 @@
+﻿using MythicalBooksAPI.Models.Auth;
+
+namespace MythicalBooksAPI.Dtos
+{
+    public class RegisterUserDto
+    {
+        public string? Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Country { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+
+        public int? PostalCode {  get; set; }
+    }
+}
