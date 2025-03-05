5e56378d0b14a51a7a053b9a4710ed33e67e031f
Debora Bucci <dbucci@protonmail.com>
2025-03-05 19:13:13 -0300

    create a controller for the authentication, and in it, create a post method to register a user

---
+++ MythicalBooksAPI/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MythicalBooksAPI.Data;
+using MythicalBooksAPI.Models.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MythicalBooksAPI.Dtos;
+using MythicalBooksAPI.Mappers;
+
+namespace MythicalBooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthContext _context;
+
+        public AuthController(AuthContext context) {
+            _context = context; 
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterUserDto request)
+        {
+
+            if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+            {
+                return BadRequest(new { message = "Unable to register user, please try again later." });
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            User user = UserMapper.ToUser(request, hashedPassword);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "User registered successfully!" });
+        }
+    }
+}
