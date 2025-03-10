9b4ef86f15b0bc555f7ac666c1cee48a2d5fd950
Debora Bucci <dbucci@protonmail.com>
2025-03-10 15:22:44 -0300

    create a TokenHelper class for generating a token with the user id and a time's life span, and checking if it's expired

---
+++ MythicalBooksAPI/Helpers/TokenHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace MythicalBooksAPI.Helpers
+{
+    public static class TokenHelper
+    {
+        private static readonly TimeSpan TokenLifeTime = TimeSpan.FromDays(1);
+
+        public static string GenerateToken(Guid userId )
+        {
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = userId.ToByteArray();
+            string token = Convert.ToBase64String(time.Concat(key).ToArray());
+
+            return token;
+        }
+
+        public static bool IsTokenExpired (string token)
+        {
+            try
+            {
+                byte[] data = Convert.FromBase64String(token);
+                DateTime tokenDate = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+                return DateTime.UtcNow - tokenDate > TokenLifeTime;
+            }
+
+            catch
+            {
+                return true;
+            }
+        }
+    }
+}
