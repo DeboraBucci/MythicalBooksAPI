117bdc342fb8078606d25738d492d408a9d3751e
Debora Bucci <dbucci@protonmail.com>
2025-04-23 17:02:49 -0300

    create a class for the json web token settings

---
+++ MythicalBooksAPI/Models/Auth/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MythicalBooksAPI.Models.Auth
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public int ExpiryMinutes { get; set; }
+    }
+}
