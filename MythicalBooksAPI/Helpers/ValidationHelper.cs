7f583256de5c3c727bde891d7c408bc326e9c04b
Debora Bucci <dbucci@protonmail.com>
2025-04-24 10:57:08 -0300

    add more methods for validation: IsNotEmpty, HasMinLength, HasMaxLength

--- MythicalBooksAPI/Helpers/ValidationHelper.cs
+++ MythicalBooksAPI/Helpers/ValidationHelper.cs
@@ -12,5 +12,20 @@ namespace MythicalBooksAPI.Helpers
             return
                  (!string.IsNullOrWhiteSpace(email) && EmailRegex.IsMatch(email));
         }
+
+        public static bool IsNotEmpty(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+
+        public static bool HasMinLength(string input, int minLength)
+        {
+            return input?.Length >= minLength;
+        }
+
+        public static bool HasMaxLength(string input, int maxLength)
+        {
+            return input?.Length <= maxLength;
+        }
     }
 }
