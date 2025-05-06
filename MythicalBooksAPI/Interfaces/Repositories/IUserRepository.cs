81eab8d95bc508cb18a24626b7ac239e3c97dcbe
Debora Bucci <dbucci@protonmail.com>
2025-05-06 15:38:11 -0300

    create interfaces

---
+++ MythicalBooksAPI/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MythicalBooksAPI.Models.Auth;
+
+namespace MythicalBooksAPI.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<bool> EmailExistsAsync(string email);
+        void AddUser(User user);
+        Task SaveChangesAsync();
+    }
+}
---
+++ MythicalBooksAPI/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using MythicalBooksAPI.Dtos.Auth;
+using MythicalBooksAPI.Helpers.Validators;
+
+namespace MythicalBooksAPI.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<ValidationResult> RegisterUserAsync(RegisterUserDto userDto);
+    }
+}
