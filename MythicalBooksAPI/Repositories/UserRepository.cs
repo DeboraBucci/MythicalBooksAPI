ab0cef65510804d815414b32f6022540064524ce
Debora Bucci <dbucci@protonmail.com>
2025-05-06 15:39:37 -0300

    create a UserRepository to handle the information about the users from the database

---
+++ MythicalBooksAPI/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Data.Contexts;
+using MythicalBooksAPI.Interfaces.Repositories;
+using MythicalBooksAPI.Models.Auth;
+
+namespace MythicalBooksAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthContext _context;
+
+        public UserRepository(AuthContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> EmailExistsAsync(string email)
+            => _context.Users.AnyAsync(u => u.Email == email);
+
+        public void AddUser(User user)
+            => _context.Users.Add(user);
+
+        public Task SaveChangesAsync()
+            => _context.SaveChangesAsync();
+    }
+}
