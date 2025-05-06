76ae77c21ecf9a0425a6cb1ce826709c1aa80bbf
Debora Bucci <dbucci@protonmail.com>
2025-05-06 12:46:29 -0300

    create a class for storing a message and a list of fields for the validation errors

---
+++ MythicalBooksAPI/Helpers/Validators/ValidationError.cs
@@ -0,0 +1,18 @@
+﻿namespace MythicalBooksAPI.Helpers.Validators
+{
+    public class ValidationError
+    {
+        public string Message { get; set; } = "";
+        public List<string>? Fields { get; set; } = null;
+
+        public ValidationError(string message)
+        {
+            Message = message;
+        }
+
+        public ValidationError(string message, List<string> fields) {
+            Message = message;
+            Fields = fields;
+        }
+    }
+}
