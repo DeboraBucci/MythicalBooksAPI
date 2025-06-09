79e248ac60fc8cad512b032793fd989dd3d8597d
Debora Bucci <dbucci@protonmail.com>
2025-06-09 11:31:22 -0300

    add middleware for validating the token before processing a request in the Controller

---
+++ MythicalBooksAPI/Middlewares/TokenValidationMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using MythicalBooksAPI.Helpers;
+
+namespace MythicalBooksAPI.Middlewares
+{
+    public class TokenValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenValidationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, TokenHelper tokenHelper)
+        {
+            var token = context
+                .Request
+                .Headers["Authorization"]
+                .FirstOrDefault()?
+                .Split(" ")
+                .Last();
+
+            if (string.IsNullOrEmpty(token) || !tokenHelper.IsTokenValid(token))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Token is invalid or expired.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
