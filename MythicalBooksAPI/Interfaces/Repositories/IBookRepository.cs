a6b916d5752f99450b65eab239be176a5e5b2abb
Debora Bucci <dbucci@protonmail.com>
2025-07-09 22:19:40 -0300

    create a repository, and its respective interface, for getting book's data

---
+++ MythicalBooksAPI/Interfaces/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MythicalBooksAPI.Dtos;
+using MythicalBooksAPI.Dtos.Books;
+using MythicalBooksAPI.Models.Books;
+
+namespace MythicalBooksAPI.Interfaces.Repositories
+{
+    public interface IBookRepository
+    {
+        IQueryable<Book> GetQueryableBooks();
+
+        Task<int> CountQueryableAsync<T>(IQueryable<T> countable);
+
+        Task<Book?> GetByIdAsync(int id);
+    }
+
+}
---
+++ MythicalBooksAPI/Repositories/BookRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Data.Contexts;
+using MythicalBooksAPI.Dtos;
+using MythicalBooksAPI.Dtos.Books;
+using MythicalBooksAPI.Interfaces.Repositories;
+using MythicalBooksAPI.Mappers;
+using MythicalBooksAPI.Models.Books;
+
+namespace MythicalBooksAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly LibraryContext _context;
+
+        public BookRepository(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Book> GetQueryableBooks() => 
+            _context
+                .Books
+                .Include(b => b.BookAuthors)
+                    .ThenInclude(ba => ba.Author)
+                .AsNoTracking()
+                .AsQueryable();
+
+        public async Task<int> CountQueryableAsync<T>(IQueryable<T> countable) => 
+            await countable.CountAsync();
+
+        public async Task<Book?> GetByIdAsync(int id) => await _context.Books
+            .AsNoTracking()
+            .Include(b => b.BookAuthors)
+                .ThenInclude(ba => ba.Author)
+            .Include(b => b.BookCategories)
+                .ThenInclude(bc => bc.Category)
+            .Include(b => b.BookPublishers)
+                .ThenInclude(bp => bp.Publisher)
+            .FirstOrDefaultAsync(b => b.Id == id);
+    }
+}
