187311d8bb8280e17f036e67ad9930540924a242
Debora Bucci <dbucci@protonmail.com>
2025-07-09 22:18:13 -0300

    create a class for returning a paged information

---
+++ MythicalBooksAPI/Dtos/PagedResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MythicalBooksAPI.Dtos
+{
+    public class PagedResultDto<T>
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 2;
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<T> Data { get; set; } = Enumerable.Empty<T>();
+    }
+}
