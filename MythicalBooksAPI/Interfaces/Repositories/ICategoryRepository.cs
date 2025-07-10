f1745e7ad412379ab597db35035b3ca3bbd3de0d
Debora Bucci <dbucci@protonmail.com>
2025-07-09 22:20:35 -0300

    create a repository, and its respective interface, for getting categories' data

---
+++ MythicalBooksAPI/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using MythicalBooksAPI.Models.Books;
+
+namespace MythicalBooksAPI.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllAsync();
+    }
+}
---
+++ MythicalBooksAPI/Repositories/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MythicalBooksAPI.Data.Contexts;
+using MythicalBooksAPI.Dtos.Books;
+using MythicalBooksAPI.Interfaces.Repositories;
+using MythicalBooksAPI.Models.Books;
+
+namespace MythicalBooksAPI.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly LibraryContext _context;
+
+        public CategoryRepository(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Category>> GetAllAsync() => _context.Categories.ToListAsync();
+    }
+}
